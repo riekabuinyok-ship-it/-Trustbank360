40e6fe4a51631bdcf8db8a91fa3e53738931bf46
Riek Abui Nyok <riekabuinyok@gmail.com>
2026-07-17 16:13:02 +0200

    fix: include favicon.ico in middleware matcher to enable redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -160,5 +160,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|public).*)"],
+  matcher: ["/((?!_next/static|_next/image|public).*)"],
 }
